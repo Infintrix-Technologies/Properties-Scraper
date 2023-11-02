130dccce16de5bd6b5417a667faf05c5f26bae9d
muqeetmughal <muqeetmughal786@gmail.com>
2023-11-02 13:57:42 +0500

    auth updated

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -169,9 +169,9 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
         # "rest_framework.authentication.SessionAuthentication",
-        # "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
