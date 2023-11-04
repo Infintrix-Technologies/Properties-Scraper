67c82d49f5e1271e03abc343e46760c18f3290b2
muqeetmughal <muqeetmughal786@gmail.com>
2023-11-04 16:12:21 +0500

    debug onn and admin urls fixed

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,13 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", lambda request : redirect("/admin") ),
     path("api/rightmove/", include("rightmove.urls")),
-    path("", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-]
-
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
