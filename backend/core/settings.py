4de79ce97681e58800796eb163710f59a550714e
muqeetmughal <muqeetmughal786@gmail.com>
2023-11-04 15:49:08 +0500

    some fix fot static files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join("static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     BASE_DIR / "assets",
     # os.path.join(BASE_DIR,'frontend/build/static'),
