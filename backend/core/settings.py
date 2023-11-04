a3dad185470d0950319a502fce251a81f330f0b5
muqeetmughal <muqeetmughal786@gmail.com>
2023-11-04 09:58:51 +0500

    debug disabled

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG") == 1 or False
 
 ALLOWED_HOSTS = ["*"]
 
