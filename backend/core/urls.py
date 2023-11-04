02c398c89d4cdf5d3308e5e21352d2fe7bf86884
muqeetmughal <muqeetmughal786@gmail.com>
2023-11-04 16:02:25 +0500

    debug on

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,13 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
 
-urlpatterns = []
+urlpatterns = [
+    # path("admin/", admin.site.urls),
+    path("api/rightmove/", include("rightmove.urls")),
+    path("", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
+]
+
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns.extend(
-    [
-        # path("admin/", admin.site.urls),
-        path("api/rightmove/", include("rightmove.urls")),
-        path("", admin.site.urls),
-        path("__debug__/", include("debug_toolbar.urls")),
-    ]
-)
